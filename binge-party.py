ed6e2809e53e6316bcbedbbcd2bbc2bf4c6efaf7
Cameron Winston <cameronwinston0413@gmail.com>
2021-07-06 19:34:31 +0000

    Provider Response Added
    Co-authored-by: Naomi Caines <ncaines@bu.edu>
    Co-authored-by: Araceli Corley <abcorley@bu.edu>

--- binge-party.py
+++ binge-party.py
@@ -1,49 +1,33 @@
 import requests
 import json
 
-# Ex API request: https://api.themoviedb.org/3/movie/550?api_key=25cd471bedf2ee053df9b1705494367d
 # menu
-"""
-print("Hello! Welcome to Binge-Party!!")
-userInput = input('Enter the name of a movie or tv show: ')
-"""
 
+print("Hello! Welcome to Binge-Party!!")
+title = input("What is the full title of the movie or show are you looking for? ")
+def switch(featureType):
+    switcher = {
+        1:'movie',
+        2:'tv'
+    }
+    return switcher.get(featureType, "Invalid choice")
+
+
+print("Is this a movie or tv show?")
+featureType = switch(int(input("Enter 1 for movie or 2 for tv show: ")))
 api_key = '25cd471bedf2ee053df9b1705494367d'
 
-response = requests.get('https://api.themoviedb.org/3/search/movie?api_key=' + api_key + '&query=Jack+Reacher')
-print(response.json())
+titleResponse = requests.get('https://api.themoviedb.org/3/search/movie?api_key=' + api_key + '&query=' + title)
+# print(titleResponse.json())
 
-# https://api.themoviedb.org/3/search/multi?api_key=<<api_key>>&language=en-US&query=%27lalalalalalalalal%27&page=1&include_adult=false
+r = titleResponse.json()
 
+for results in r['results']:
+    print(results['title'], results['release_date'], "ID:", results['id'])
+resultID = input("Type in the id of the movie/show you're interested in: ")
 
-{'page': 1, 'results': [
-    {'adult': False, 
-     'backdrop_path': '/k7h4RNAarfOrF2r2YMN0P2FQSr4.jpg', 
-     'genre_ids': [80, 18, 53, 28], 
-     'id': 75780, 
-     'original_language': 
-     'en', 'original_title': 
-     'Jack Reacher', 'overview': 'When a gunman takes five lives with six shots, all evidence points to the suspect in custody. On interrogation, the suspect offers up a single note: "Get Jack Reacher!" So begins an extraordinary chase for the truth, pitting Jack Reacher against an unexpected enemy, with a skill for violence and a secret to keep.', 
-     'popularity': 33.1, 
-     'poster_path': '/zlyhKMi2aLk25nOHnNm43MpZMtQ.jpg', 
-     'release_date': '2012-12-20', 
-     'title': 'Jack Reacher', 
-     'video': False, 
-     'vote_average': 6.5, 
-     'vote_count': 5292
-    }, 
-    {'adult': False, 
-     'backdrop_path': '/ww1eIoywghjoMzRLRIcbJLuKnJH.jpg', 
-     'genre_ids': [28], 'id': 343611, 
-     'original_language': 'en', 
-     'original_title': 'Jack Reacher: Never Go Back', 
-     'overview': 'Jack Reacher must uncover the truth behind a major government conspiracy in order to clear his name. On the run as a fugitive from the law, Reacher uncovers a potential secret from his past that could change his life forever.', 'popularity': 42.551, 'poster_path': '/wxLUQ1pIms3HAlVGYvEG9zg2kDs.jpg', 
-     'release_date': '2016-10-19', 
-     'title': 'Jack Reacher: Never Go Back', 
-     'video': False, 
-     'vote_average': 5.8, 
-     'vote_count': 3631
-    }
-], 
- 'total_pages': 1, 
- 'total_results': 2}
+provResponse = requests.get('https://api.themoviedb.org/3/' + featureType + '/' + resultID + '/watch/providers?api_key=' + api_key)
+# print(provResponse.json())
+# pr = provResponse.json()
+# for res in pr['results']:
+# print(res['provider_name'])
