4083da6895cd18f96e25d8fd347971b6a0cd8622
Cameron Winston <cameronwinston0413@gmail.com>
2021-07-07 02:45:59 +0000

    added functions
    Co-authored-by: Naomi Caines <ncaines@bu.edu>
    Co-authored-by: Araceli Corley <abcorley@bu.edu>

--- binge-party.py
+++ binge-party.py
@@ -1,10 +1,14 @@
 import requests
 import json
 
-# menu
+def menu():
+    #print("Hello! Welcome to Binge-Party!!")
+    pass
 
-print("Hello! Welcome to Binge-Party!!")
-title = input("What is the full title you looking for? ")
+
+def getFeatureTitle():
+    title = input("What is the full title you looking for? ")
+    return title
 
 
 def switch(featureType):
@@ -15,30 +19,41 @@ def switch(featureType):
     return switcher.get(featureType, "Invalid choice")
 
 
-print("Is this a movie or tv show?")
-featureType = switch(int(input("Enter 1 for movie or 2 for tv show: ")))
-api_key = '25cd471bedf2ee053df9b1705494367d'
+def getFeatureType():
+    print("Is this a movie or tv show?")
+    featureType = switch(int(input("Enter 1 for movie or 2 for tv show: ")))
+    return featureType
 
-titleResponse = requests.get(
-                'https://api.themoviedb.org/3/search/movie?api_key='
-                + api_key + '&query=' + title
-                )
 
-r = titleResponse.json()
+def getTitleJSONData(api_key, title, featureType):
+    titleResponse = requests.get(
+                    'https://api.themoviedb.org/3/search/' +
+                     featureType +
+                    '?api_key='+ api_key + '&query=' + title
+                    )
+    r = titleResponse.json()
+    return r
 
-for results in r['results']:
-    print(results['title'], results['release_date'], "ID:", results['id'])
-resultID = input("Type in the id of the movie/show you're interested in: ")
 
-provResponse = requests.get(
-                'https://api.themoviedb.org/3/'
-                + featureType + '/' + resultID
-                + '/watch/providers?api_key=' + api_key
-                )
-# print(provResponse.json())
-pr = provResponse.json()
+def getFeatureID(r, featureType):
+    if featureType == 'movie':
+        for results in r['results']:
+            print(results['title'], results['release_date'], "ID:", results['id'])
+    else:
+        for results in r['results']:
+            print(results['name'], "ID:", results['id'])
+    resultID = input("Type in the id of the movie/show you're interested in: ")
+    return resultID
 
-print('1).Streaming Services\n2).Rent\n3).Buy')
+
+def getProvJsonData(featureType, resultID, api_key):
+    provResponse = requests.get(
+                    'https://api.themoviedb.org/3/'
+                    + featureType + '/' + resultID
+                    + '/watch/providers?api_key=' + api_key
+                    )
+    pr = provResponse.json()
+    return pr
 
 
 def switch2(buyOption):
@@ -50,6 +65,28 @@ def switch2(buyOption):
     return switcher.get(buyOption, "No listings available for this option.")
 
 
-buyOption = switch2(int(input("Enter 1 for streaming service options, 2 for rent options, or 3 for buy options. ")))
-for results in pr['results']['US'][buyOption]:
-    print(results['provider_name'])
+def getBuyOption():
+    #print('1).Streaming Services\n2).Rent\n3).Buy')
+    buyOption = switch2(int(input("Enter 1 for streaming service options, 2 for rent options, or 3 for buy options. ")))
+    return buyOption
+
+
+def printProvResults(pr, buyOption, featureType):
+    print("Here are all of the platforms you can find this on: ")
+    for results in pr['results']['US'][buyOption]:
+        print(results['provider_name'])
+
+
+def main():
+    api_key = '25cd471bedf2ee053df9b1705494367d'
+    search = getFeatureTitle()
+    typ = getFeatureType()
+    resp = getTitleJSONData(api_key, search, typ)
+    ID = getFeatureID(resp, typ)
+    resp2 = getProvJsonData(typ, ID, api_key)
+    buyOp = getBuyOption()
+    printProvResults(resp2, buyOp, typ)
+
+
+if __name__ == "__main__":
+    main()
